255e22bd5b46cb5e2675c31f1ead3ca3b05c0036
KusYourHeart <iiwonderwhyii@gmail.com>
2023-10-12 15:05:56 +0300

    Слияние изменений из time

--- components/features/DropdownMenu.tsx
+++ components/features/DropdownMenu.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import styles from '@/styles/dropdownmenu.module.scss';
 
+interface NavigationItem {
+  id: number;
+  title: string;
+  path: string;
+}
 
-const navigation = [
+const navigation: NavigationItem[] = [
   { id: 1, title: 'About us', path: 'About' },
   { id: 2, title: 'Technologies', path: 'Tech' },
   { id: 3, title: 'Contacts', path: 'contacts' },
 ];
 
-const DropdownMenu = ({ navigation, onClose }) => {
+const DropdownMenu = ({ navigation, onClose }: { navigation: NavigationItem[], onClose: () => void }) => {
   return (
     <div className={styles.dropdownmenu}>
       <ul>
--- components/features/ModalEmail.tsx
+++ components/features/ModalEmail.tsx
@@ -3,38 +3,50 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import styles from '@/styles/email.module.scss';
-import transporter from './Mailer'; // Импортируйте transporter из mailer.js
 
 function Email() {
   const [show, setShow] = useState(false);
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    const mailOptions = {
-      from: email,
-      to: 'vincent.freeman@list.ru', // Замените на адрес получателя
-      subject: 'Сообщение с вашего сайта',
-      text: message,
-    };
 
+    const recipientEmail = 'iiwonderwhyii@gmail.com';
+    // Ваша логика отправки данных, например, используя fetch или axios.
+    const data = { email, message, recipientEmail };
+
+    // Отправка данных на сервер
     try {
-      await transporter.sendMail(mailOptions); // Отправляем сообщение с использованием Nodemailer
-      alert('Сообщение успешно отправлено');
-      handleClose();
+      const response = await fetch('/api/contact', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        
+      }); 
+
+      if (response.ok) {
+        // Успешная отправка
+        alert('Сообщение успешно отправлено');
+        handleClose();
+      } else {
+        // Ошибка при отправке
+        alert('Произошла ошибка при отправке сообщения');
+      }
     } catch (error) {
-      console.error('Ошибка отправки сообщения:', error);
-      alert('Произошла ошибка при отправке сообщения');
+      console.error('Произошла ошибка:', error);
     }
   };
 
   return (
+
+    
     <>
+
+    
       <Button variant="" onClick={handleShow} className="btn btn-outline-dark">
         Contact us
       </Button>
